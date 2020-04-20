e28e2e8dafb8b734cf172687554c0d8a1037e883
anastasiya-zhyrkevich <anastasiya.zhyrkevich@yandex.ru>
2020-04-20 19:04:23 +0300

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+const faker = require('faker');
+const puppeteer = require('puppeteer');
+const assert = require('assert');
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const person = {
+  name: faker.name.firstName() + ' ' + faker.name.lastName(),
+  email: faker.internet.email(),
+  phone: faker.phone.phoneNumber(),
+  message: faker.random.words()
+};
+
+describe('H1 Text', () => {
+  it('h1 loads correctly', async () => {
+    browser = await puppeteer.launch({
+      args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+      ]
+    });
+    page = await browser.newPage();
+    try {
+      await page.goto('http://localhost:3000/', { waitUntil: 'load' });
+    } catch(err) {
+      console.warn(err);
+    }
+
+    // =======================
+    {
+      await page.waitForSelector('input', { timeout: 1500 });
+
+      let header = await page.$x('//h1');
+      assert.equal(header[0].innerHTML, 'Search Users');
+
+      
+      // assert.equal(autocompleteText, ' ');
+    }
+    // ========================  
+
+
+
+    browser.close();
+  }, 16000);
 });
