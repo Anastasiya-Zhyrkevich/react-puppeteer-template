280ca6fea96409b0a537c173649ab1cbb410eb33
anastasiya-zhyrkevich <anastasiya.zhyrkevich@yandex.ru>
2020-05-12 14:08:34 +0300

    try catch

--- react/src/ProgressBar/utils.js
+++ react/src/ProgressBar/utils.js
@@ -16,11 +16,21 @@ async function _formProgressBarWidthInRange(minBound, maxBound) {
 async function checkFormProgressBarWidthInRange(page, minBound, maxBound) {
   await page.waitForSelector('.form-progress-bar-wrapper', { timeout: 10 });
   await page.waitForSelector('.form-progress-bar', { timeout: 10 });
-  await page.waitForFunction(
-    _formProgressBarWidthInRange,
-    { timeout: 200 },
-    minBound, maxBound
-  );
+  try {
+    await page.waitForFunction(
+      _formProgressBarWidthInRange,
+      { timeout: 200 },
+      minBound, maxBound
+    ); 
+  } catch(error) {
+     const p = await page.evaluate(() => {
+      const progressWidth = document.querySelector('.form-progress-bar-wrapper').getBoundingClientRect().width;
+      const formWidth = document.querySelector('.form-progress-bar').getBoundingClientRect().width;
+      return 1. * formWidth / progressWidth;
+     });
+     assert.fail(`.form-progress-bar width is ${p * 100}% of .form-progress-bar-wrapper, ` +
+                 `but expected to be in range [${minBound * 100}%, ${maxBound * 100}%]`);
+  }
 }
 
 module.exports = {
